52f46f258b2c95f148469d86d283959270690680
vbhv2219 <kaushikvaibhav82@gmail.com>
2024-07-31 22:58:31 +0530

    Files upload

---
+++ formula/gui.py
@@ -0,0 +1,248 @@
+import os
+import sys
+import shutil
+import threading
+import pandas as pd
+from random import randint
+from PyQt5 import QtGui
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QComboBox, QApplication, QWidget, QVBoxLayout, QHBoxLayout,QLabel, QPushButton, QProgressBar, QMessageBox
+
+# create directories
+CWD = os.getcwd()
+if not os.path.exists(CWD + '/cache'):
+    cur_dir = CWD + '/'
+    main_dirs = ['cache', 'plot']
+    for i in main_dirs:
+        os.makedirs(cur_dir + i)
+
+# imports script.py, used for creating plots
+import script
+
+# paths for race data
+events = pd.read_csv(CWD + '/formula/data/events.csv')
+drivers = pd.read_csv(CWD + '/formula/data/drivers.csv')
+race_laps = pd.read_csv(CWD + '/formula/data/laps.csv')
+placeholder_path = CWD + '/formula/img/placeholder.png'
+
+# active race years
+year = events.columns
+year = year[1:len(year)].to_list()
+year.insert(0, 'Select Year')
+
+# values for dropdown lables
+driver_name = drivers
+location = ['Select Location']
+session = ['Race', 'Qualifying', 'FP1', 'FP2', 'FP3']# 'Sprint Qualifying', 'Sprint' : removed until data is consistent
+driver_name = ['Select Driver']
+analysis_type = ['Lap Time', 'Fastest Lap', 'Fastest Sectors', 'Full Telemetry']
+
+# stylesheet for progress bar
+StyleSheet = '''
+#RedProgressBar {
+    min-height: 12px;
+    max-height: 12px;
+    border-radius: 2px;
+    border: .5px solid #808080;;
+}
+#RedProgressBar::chunk {
+    border-radius: 2px;
+    background-color: #DC0000;
+    opacity: 1;
+}
+.warning-text {
+    color:#DC0000
+}
+'''
+
+# defines progressbar
+class ProgressBar(QProgressBar):
+    def __init__(self, *args, **kwargs):
+        super(ProgressBar, self).__init__(*args, **kwargs)
+        self.setValue(0)
+        if self.minimum() != self.maximum():
+            self.timer = QTimer(self, timeout=self.onTimeout)
+            self.timer.start(randint(1, 3) * 1000)
+   
+    # timeout isn't currently necessary, but useful if behavior needs to change (if you want bar to appear for specific time)
+    def onTimeout(self):
+        if self.value() >= 100:
+            self.timer.stop()
+            self.timer.deleteLater()
+            del self.timer
+            return
+        self.setValue(self.value() + 1)
+
+# main gui window 
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.UIComponents()
+
+    # initialize main window
+    def initUI(self):
+        self.setFixedSize(880, 525)
+        self.move(200, 100)
+        self.setWindowTitle('Formula 1 Telemetry Analytics')
+        self.setWindowIcon(QtGui.QIcon(CWD + '/formula/img/f1.png'))
+
+    # creates and places all window compenenets, including listeners
+    def UIComponents(self):
+        options_layout = QVBoxLayout()
+        img_layout = QHBoxLayout()
+        img_layout.addLayout(options_layout) # two layouts to allow split screen view
+
+        self.drop_year = QComboBox()
+        self.drop_grand_prix = QComboBox()
+        self.drop_session = QComboBox()
+        self.drop_driver1 = QComboBox()
+        self.drop_driver2 = QComboBox()
+        self.drop_analysis = QComboBox()
+        self.lap_number = QComboBox()
+
+        # not working properly
+        self.warning_box = QMessageBox(self)
+        self.warning_box.setWindowTitle('Error!')
+        self.warning_box.setText('Select a valid race year.')
+        self.warning_box.setDefaultButton(QMessageBox.Ok)
+
+        label_year = QLabel('<span style="font-size:8.5pt; font-weight: 500"> Year: </span>')
+        label_prix = QLabel('<span style="font-size:8.5pt; font-weight: 500"> Grand Prix Location: </span>') 
+        label_session = QLabel('<span style="font-size:8.5pt; font-weight: 500"> Session: </span>')
+        label_d1 = QLabel('<span style="font-size:8.5pt; font-weight: 500"> Driver 1: </span>')
+        label_d2 = QLabel('<span style="font-size:8.5pt; font-weight: 500"> Driver 2: </span>')
+        label_analysis = QLabel('<span style="font-size:8.5pt; font-weight: 500"> Analysis Type: </span>')
+
+        self.run_button = QPushButton('Run Analysis')
+        self.save_button = QPushButton('Save Plot to Desktop')
+
+        self.pbar = ProgressBar(self, minimum=0, maximum=0, textVisible=False,
+                                objectName="RedProgressBar")
+
+        self.drop_year.addItems(year)
+        self.drop_grand_prix.addItems(location)
+        self.drop_session.addItems(session)
+        self.drop_driver1.addItems(driver_name)
+        self.drop_driver2.addItems(driver_name)
+        self.drop_analysis.addItems(analysis_type)
+
+        options_layout.addWidget(label_year)
+        options_layout.addWidget(self.drop_year)
+        options_layout.addWidget(label_prix)
+        options_layout.addWidget(self.drop_grand_prix)
+        options_layout.addWidget(label_session)
+        options_layout.addWidget(self.drop_session)
+        options_layout.addWidget(label_d1)
+        options_layout.addWidget(self.drop_driver1)
+        options_layout.addWidget(label_d2)
+        options_layout.addWidget(self.drop_driver2)
+        options_layout.addWidget(label_analysis)
+        options_layout.addWidget(self.drop_analysis)
+        options_layout.addWidget(self.lap_number)
+        self.lap_number.hide()
+        options_layout.addWidget(self.pbar)
+        self.pbar.hide()
+        options_layout.addWidget(self.run_button)
+        options_layout.addWidget(self.save_button)
+        self.save_button.hide()
+        
+        options_layout.addStretch() # compacts all widgets
+
+        self.drop_year.currentTextChanged.connect(self.update_lists) # listens for change in year
+        self.run_button.clicked.connect(self.thread_script) # listens for run analysis button press
+        self.save_button.clicked.connect(self.save_plot) # listens for save button press
+        self.drop_analysis.currentTextChanged.connect(self.add_laps) # listens to add lap selection
+        self.drop_grand_prix.currentTextChanged.connect(self.update_laps) # updates lap number based on grand prix selection
+
+        self.img_plot = QLabel()
+        self.img_plot.setPixmap(QPixmap(placeholder_path).scaledToWidth(625)) # could increase scale to improve readability of high dpi images
+        img_layout.addWidget(self.img_plot)
+
+        self.setLayout(img_layout)
+        
+    # returns list of user selections (year, location, driver 1, driver 2, analysis type)
+    def current_text(self):
+        input_data = []
+        text = self.drop_year.currentText()
+        input_data.append(text)
+        text = self.drop_grand_prix.currentText()
+        input_data.append(text)
+        text = self.drop_session.currentText()
+        input_data.append(text)
+        text = self.drop_driver1.currentText()
+        input_data.append(text)
+        text = self.drop_driver2.currentText()
+        input_data.append(text)
+        text = self.drop_analysis.currentText()
+        input_data.append(text)
+        text = self.lap_number.currentText()
+        input_data.append(text)
+        return input_data
+
+    # displays requested analysis plot, returned from script.py
+    def display_plot(self, plot_path):
+        self.img_plot.setPixmap(QPixmap(plot_path).scaledToWidth(625))
+
+    # saves currently displayed plot to user's desktop as .png
+    def save_plot(self):
+        desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') # may need to adjust for mac
+        shutil.copy(self.plot_path, desktop_path)
+
+    # adds option to select the lap number for sector analysis
+    def add_laps(self):
+        if self.drop_analysis.currentText() == 'Fastest Sectors':
+            self.lap_number.show()
+        else:
+            self.lap_number.hide()
+
+    # updates total number of laps depending on grand prix selection
+    def update_laps(self):
+        if self.drop_grand_prix.currentText() != '':
+            self.lap_number.clear()
+            lap_val = ['Select Lap']
+            race = self.drop_grand_prix.currentText()
+            total_laps = race_laps.loc[race_laps.event == race, 'laps'].values[0]
+            lap_val.extend(range(1, total_laps + 1))
+            lap_val = map(str, lap_val)
+            self.lap_number.addItems(lap_val)
+
+    # starts new thread for script.py operation so gui.py does not freeze
+    def thread_script(self):
+        thread_script = threading.Thread(target = self.button_listen)
+        thread_script.start()
+
+    # activates on analysis button press, gets input_data, runs script.py, adjusts gui. checks if year value is valid
+    def button_listen(self):
+        input_data = self.current_text()
+        if input_data[0] == 'Select Year':
+            self.run_button.setText('Run Analysis (Select Valid Year)')
+        else:
+            self.run_button.setText('Running . . .')
+            self.save_button.hide()
+            self.pbar.show()
+            script.get_race_data(input_data)
+            self.plot_path = os.getcwd() + (f'/formula/plot/{input_data[5]}.png')
+            self.display_plot(self.plot_path)
+            self.pbar.hide()
+            self.run_button.setText('Run New Analysis')
+            self.save_button.show()
+    
+    # updates all selection lists based on the year selected. drivers/race locations change each year
+    def update_lists(self):
+        sel_year = self.drop_year.currentText()
+        if sel_year != 'Select Year':
+            self.drop_grand_prix.clear()
+            self.drop_driver1.clear()
+            self.drop_driver2.clear()
+            self.drop_grand_prix.addItems(events[str(sel_year)].dropna().to_list())
+            self.drop_driver1.addItems(drivers[str(sel_year)].dropna().to_list())
+            self.drop_driver2.addItems(drivers[str(sel_year)].dropna().to_list())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet(StyleSheet)
+    mw = MainWindow()
+    mw.show()
+    sys.exit(app.exec_())
---
+++ formula/script.py
@@ -0,0 +1,237 @@
+# imports
+import os
+import matplotlib
+import numpy as np
+import pandas as pd
+import fastf1 as ff1
+from fastf1 import api
+from fastf1 import utils
+from fastf1 import plotting
+from matplotlib.lines import Line2D
+from matplotlib import pyplot as plt
+from matplotlib.collections import LineCollection
+
+# enables cache, allows storage of race data locally
+ff1.Cache.enable_cache('cache')
+
+# patches matplotlib for time delta support
+ff1.plotting.setup_mpl(mpl_timedelta_support = True, color_scheme = 'fastf1')
+
+# gets race data from fastf1 based on input data parameter
+# runs appropriate plot function based on user input
+def get_race_data(input_data):
+    #['2022', 'Austria', 'FP1', 'VER', 'VER', 'Lap Time']
+    race = ff1.get_session(int(input_data[0]), input_data[1], input_data[2])
+    race.load()
+
+    if input_data[5] == 'Lap Time':
+        plot_laptime(race, input_data)
+    elif input_data[5] == 'Fastest Lap':
+        plot_fastest_lap(race, input_data)
+    elif input_data[5] == 'Fastest Sectors':
+        plot_fastest_sectors(race, input_data)
+    elif input_data[5] == 'Full Telemetry':
+        plot_full_telemetry(race, input_data)
+
+# takes in speed/distance data for both drivers and determines which is faster
+# returns dataframe of which driver was the fastest in each sector
+def get_sectors(average_speed, input_data):
+    sectors_combined = average_speed.groupby(['Driver', 'Minisector'])['Speed'].mean().reset_index()
+    final = pd.DataFrame({
+        'Driver': [],
+        'Minisector': [],
+        'Speed': []
+    })
+
+    d1 = sectors_combined.loc[sectors_combined['Driver'] == input_data[3].split()[0]]
+    d2 = sectors_combined.loc[sectors_combined['Driver'] == input_data[4].split()[0]]
+
+    for i in range(0, len(d1)): #issue, sometimes length of d1 is not 25
+        d1_sector = d1.iloc[[i]].values.tolist()
+        d1_speed = d1_sector[0][2]
+        d2_sector = d2.iloc[[i]].values.tolist()
+        d2_speed = d2_sector[0][2]
+        if d1_speed > d2_speed:
+            final.loc[len(final)] = d1_sector[0]
+        else:
+            final.loc[len(final)] = d2_sector[0]
+
+    return final
+
+# plots a laptime/distance comparison for both specified drivers
+# returns a saved version of the generated plot
+def plot_laptime(race, input_data):
+    plt.clf()
+    d1 = input_data[3].split()[0]
+    d2 = input_data[4].split()[0]
+
+    laps_d1 = race.laps.pick_driver(d1)
+    laps_d2 = race.laps.pick_driver(d2)
+
+    color1 = ff1.plotting.driver_color(input_data[3])
+    color2 = ff1.plotting.driver_color(input_data[4])
+
+    fig, ax = plt.subplots()
+    ax.plot(laps_d1['LapNumber'], laps_d1['LapTime'], color = color1, label = input_data[3])
+    ax.plot(laps_d2['LapNumber'], laps_d2['LapTime'], color = color2, label = input_data[4])
+    ax.set_xlabel('Lap Number')
+    ax.set_ylabel('Lap Time')
+    ax.legend()
+    plt.suptitle(f"Lap Time Comparison \n" f"{race.event.year} {race.event['EventName']} {input_data[2]}")
+
+    img_path = os.getcwd() + (f'/formula/plot/{input_data[5]}.png')
+    plt.savefig(img_path, dpi = 200)
+
+# speed comaprison by distance for the fastest lap of both drivers
+# returns a saved version of the generated plot
+def plot_fastest_lap(race, input_data):
+    plt.clf()
+    d1 = input_data[3].split()[0]
+    d2 = input_data[4].split()[0]
+
+    fastest_d1 = race.laps.pick_driver(d1).pick_fastest()
+    fastest_d2 = race.laps.pick_driver(d2).pick_fastest()
+
+    tel_d1 = fastest_d1.get_car_data().add_distance()
+    tel_d2 = fastest_d2.get_car_data().add_distance()
+
+    color1 = ff1.plotting.driver_color(input_data[3])
+    color2 = ff1.plotting.driver_color(input_data[4])
+
+    fig, ax = plt.subplots()
+    ax.plot(tel_d1['Distance'], tel_d1['Speed'], color = color1, label = input_data[3])
+    ax.plot(tel_d2['Distance'], tel_d2['Speed'], color = color2, label = input_data[4])
+    ax.set_xlabel('Distance (m)')
+    ax.set_ylabel('Speed (km/h)')
+    ax.legend()
+    plt.suptitle(f"Fastest Lap Comparison \n" f"{race.event.year} {race.event['EventName']} {input_data[2]}")
+
+    img_path = os.getcwd() + (f'/formula/plot/{input_data[5]}.png')
+    plt.savefig(img_path, dpi = 700)
+
+
+# compares the sector speeds for each driver, and generates a map of the circuit, with color coded sectors for the fastest driver.
+# returns a saved version of the generated plot
+def plot_fastest_sectors(race, input_data):
+    plt.clf()
+    laps = race.laps
+    drivers = [input_data[3].split()[0], input_data[4].split()[0]]
+    telemetry = pd.DataFrame()
+    
+    # list of each driver
+    for driver in drivers:
+        driver_laps = laps.pick_driver(driver)
+
+        # gets telemetry data for each driver on each lap
+        for lap in driver_laps.iterlaps():
+            driver_telemtry = lap[1].get_telemetry().add_distance()
+            driver_telemtry['Driver'] = drivers
+            driver_telemtry['Lap'] = lap[1]['LapNumber']
+
+            telemetry = telemetry.append(driver_telemtry)
+
+    # keeping important columns
+    telemetry = telemetry[['Lap', 'Distance', 'Driver', 'Speed', 'X', 'Y']]
+
+    # creating minisectors
+    total_minisectors = 25
+    telemetry['Minisector'] = pd.cut(telemetry['Distance'], total_minisectors, labels = False) + 1
+    
+    average_speed = telemetry.groupby(['Lap', 'Minisector', 'Driver'])['Speed'].mean().reset_index()
+    
+    # calls function to returns fastest driver in each sector
+    best_sectors = get_sectors(average_speed, input_data)
+    best_sectors = best_sectors[['Driver', 'Minisector']].rename(columns = {'Driver': 'fastest_driver'})
+
+    # merges telemetry df with minisector df
+    telemetry = telemetry.merge(best_sectors, on = ['Minisector'])
+    telemetry = telemetry.sort_values(by = ['Distance'])
+
+    telemetry.loc[telemetry['fastest_driver'] == input_data[3].split()[0], 'fastest_driver_int'] = 1
+    telemetry.loc[telemetry['fastest_driver'] == input_data[4].split()[0], 'fastest_driver_int'] = 2
+    
+    # gets x,y data for a single lap. useful for drawing circuit.
+    # x,y values can be inconsistent, causing strange behavior.
+    single_lap = telemetry.loc[telemetry['Lap'] == int(input_data[6])]
+    lap_x = np.array(single_lap['X'].values)
+    lap_y = np.array(single_lap['Y'].values)
+
+    # points and segments for drawing lap
+    points = np.array([lap_x, lap_y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    # grabs which driver (1/2) is fastest # POTENTIAL PROBLEM, ENSURE THIS IS BEST SECTOR DATA
+    which_driver = single_lap['fastest_driver_int'].to_numpy().astype(float)
+
+    # getting colormap for two drivers
+    color1 = ff1.plotting.driver_color(input_data[3])
+    color2 = ff1.plotting.driver_color(input_data[4])
+    color1 = matplotlib.colors.to_rgb(color1)
+    color2 = matplotlib.colors.to_rgb(color2)
+    colors = [color1, color2]
+    cmap = matplotlib.colors.ListedColormap(colors)
+
+    lc_comp = LineCollection(segments, norm = plt.Normalize(1, cmap.N), cmap = cmap)
+    lc_comp.set_array(which_driver)
+    lc_comp.set_linewidth(2)
+
+    plt.rcParams['figure.figsize'] = [6.25, 4.70]
+    plt.suptitle(f"Average Fastest Sectors Lap {input_data[6]}\n" f"{race.event.year} {race.event['EventName']} {input_data[2]}") #edit
+    plt.gca().add_collection(lc_comp)
+    plt.axis('equal')
+    plt.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
+
+    legend_lines = [Line2D([0], [0], color = color1, lw = 1),
+                    Line2D([0], [0], color = color2, lw = 1)]
+
+    plt.legend(legend_lines, [input_data[3], input_data[4]])
+
+    img_path = os.getcwd() + (f'/formula/plot/{input_data[5]}.png')
+    plt.savefig(img_path, dpi = 200)
+
+# plots a speed, throttle, brake, rpm, gear, and drs comparison for both drivers
+# returns a saved version of the generated plot
+def plot_full_telemetry(race, input_data): # speed, throttle, brake, rpm, gear, drs 
+    plt.clf()
+    d1 = input_data[3].split()[0]
+    d2 = input_data[4].split()[0]
+
+    fastest_d1 = race.laps.pick_driver(d1).pick_fastest()
+    fastest_d2 = race.laps.pick_driver(d2).pick_fastest()
+
+    tel_d1 = fastest_d1.get_car_data().add_distance()
+    tel_d1['Brake'] = tel_d1['Brake'].astype(int)
+    tel_d2 = fastest_d2.get_car_data().add_distance()
+    tel_d2['Brake'] = tel_d2['Brake'].astype(int)
+
+    delta_time, ref_tel, compare_tel = utils.delta_time(fastest_d1, fastest_d2)
+
+    telem_data_combined = [tel_d1, tel_d2]
+    colors = [ff1.plotting.driver_color(input_data[3]), ff1.plotting.driver_color(input_data[4])]
+
+    fig, ax = plt.subplots(6)
+    for telem, color in zip(telem_data_combined, colors):
+        ax[0].axhline(0, color = 'White', linewidth = .50)
+        ax[0].plot(ref_tel['Distance'], delta_time, color = color, linewidth = .75)
+        ax[1].plot(telem['Distance'], telem['Speed'], color = color, linewidth = .75)
+        ax[2].plot(telem['Distance'], telem['Throttle'], color = color, linewidth = .75)
+        ax[3].plot(telem['Distance'], telem['Brake'], color = color, linewidth = .75) # might have to convert to binary 
+        ax[4].plot(telem['Distance'], telem['RPM'], color = color, linewidth = .75)
+        ax[5].plot(telem['Distance'], telem['nGear'], color = color, linewidth = .75)
+
+    ax[0].set(ylabel = 'Delta (s)')
+    ax[1].set(ylabel = 'Speed')
+    ax[2].set(ylabel = 'Throttle')
+    ax[3].set(ylabel = 'Brake')
+    ax[4].set(ylabel = 'RPM')
+    ax[5].set(ylabel = 'Gear')
+
+    plt.suptitle(f"Fastest Lap Telemetry - {input_data[3]} vs {input_data[4]} \n {race.event.year} {race.event['EventName']} {input_data[2]}")
+
+    legend_lines = [Line2D([0], [0], color = colors[0], lw = 1),
+        Line2D([0], [0], color = colors[1], lw = 1)]
+
+    ax[0].legend(legend_lines, [input_data[3], input_data[4]], loc = 'lower right', prop={'size': 5})
+
+    img_path = os.getcwd() + (f'/formula/plot/{input_data[5]}.png')
+    plt.savefig(img_path, dpi = 200)
